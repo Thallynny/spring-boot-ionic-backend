8deecbb9d1387775493d59d0d25f0f8adaa0c9ff
Thallynny <thallynnyemmanuelly@gmail.com>
2019-11-08 16:01:15 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/curso/domain/Endereco.java
+++ src/main/java/com/curso/domain/Endereco.java
@@ -2,12 +2,15 @@ package com.curso.domain;
 
 import java.io.Serializable;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity 
 public class Endereco implements Serializable {
@@ -22,6 +25,7 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
---
+++ src/main/java/com/curso/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.curso.services;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.curso.domain.Cliente;
+import com.curso.repositories.ClienteRepository;
+import com.curso.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+	}
+
